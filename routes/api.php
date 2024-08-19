fc2be57a630390955f9c9a2e714522d4bdf89ad1
obedah shaban <95497072+obedah96@users.noreply.github.com>
2024-08-19 03:48:53 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,18 @@ use App\Http\Controllers\CourcesTimeController;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('blogs',[BlogsController::class,'create']);
 Route::get('blogs',[BlogsController::class,'index']);
-Route::post('blogs',[BlogsController::class,'store']);
-Route::delete('blogs',[BlogsController::class,'destroy']);
 Route::post('blogs/update',[BlogsController::class,'update']);
+Route::delete('blogs',[BlogsController::class,'destroy']);
 
- 
+
+ Route::post('subscribers',[SubscribersController::class,'create']);
 Route::get('subscribers',[SubscribersController::class,'index']);
-Route::post('subscribers',[SubscribersController::class,'store']);
 Route::post('subscribers/update',[SubscribersController::class,'update']);
 Route::delete('subscribers',[SubscribersController::class,'destroy']);
+
+
 
 Route::post('cources',[CourcesController::class,'create']);
 Route::get('cources',[CourcesController::class,'index']);
